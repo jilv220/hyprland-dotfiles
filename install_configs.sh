7fbc6d8d9d58e821d01da824e0258d5177174f54
jilv220 <hkfhfkcoc@protonmail.com>
2023-09-12 11:08:48 -0700

    add cursor themes

--- install_configs.sh
+++ install_configs.sh
@@ -21,5 +21,9 @@ cp -r .icons/ ~
 echo 'Apply Tela Circle Icon...'
 gsettings set org.gnome.desktop.interface icon-theme "Tela circle dracula"
 
+echo 'Apply Bibata Modern cursor theme...'
+gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Classic'
+gsettings set org.gnome.desktop.interface cursor-size 20
+
 echo 'Install fcitx5 theme...'
 cp -r ./fcitx5 ~/.local/share
