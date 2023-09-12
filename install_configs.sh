940587d345e21abcbe420dd1b1bb806be8750750
jilv220 <hkfhfkcoc@protonmail.com>
2023-09-12 10:14:34 -0700

    update applying themes

--- install_configs.sh
+++ install_configs.sh
@@ -7,5 +7,18 @@ waybar-hyprland wlogout rofi thunar{,-archive-plugin} \ --needed
 echo 'Install config files...'
 cp -r .config/ ~
 
+echo 'Install themes files...'
+cp -r .themes/ ~
+
+echo 'Apply Dracula theme...'
+gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
+gsettings set org.gnome.desktop.wm.preferences theme "Dracula"
+
+echo 'Install icons files...'
+cp -r .icons/ ~
+
+echo 'Apply Tela Circle Icon...'
+gsettings set org.gnome.desktop.interface icon-theme "Tela circle dracula"
+
 echo 'Install fcitx5 theme...'
 cp -r ./fcitx5 ~/.local/share
--- update_configs.sh
+++ update_configs.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
 cp -r ~/.config/{hypr,waybar,swaylock,rofi,wlogout,alacritty,dunst} ./.config
-cp -r ~/.themes/ ./.themes
